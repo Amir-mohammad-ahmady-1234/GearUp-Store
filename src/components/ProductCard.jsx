34b73d1d95463074e44b55a559b1af6bff17df65
amirMohammad <amyrmhmdahmdy179@gmail.com>
2025-04-06 09:31:48 -0700

    add project image

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 const ProductCard = () => {
     return (
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-        <img src="https://via.placeholder.com/200" alt="Product" className="w-full h-48 object-cover" />
+        <img src="images/alpina-01.jpg" alt="Product" className="w-full h-48 object-cover" />
         <div className="p-4">
           <h3 className="text-lg font-semibold">Product Name</h3>
           <p className="text-gray-600">Description of the product.</p>
